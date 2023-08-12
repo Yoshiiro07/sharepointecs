71cf13ca9bcd65e9c231c5b1d5dc069c003fcd8c
Yoshiiro <59020911+Yoshiiro07@users.noreply.github.com>
2023-08-12 19:20:22 -0300

    Update com logs
    Update com logs

--- sharepointecs/sharepointecs/AppRun.cs
+++ sharepointecs/sharepointecs/AppRun.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace sharepointecs
 {
@@ -30,27 +31,38 @@ namespace sharepointecs
         {
             try
             {
+                //Set log
+                Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+
+                Log.Information("Iniciando a operação!");
                 //Check Pages Status
                 var pagesFromDB = _controlDBRepository.GetListControlDB();
                 if (pagesFromDB.Count() > 0)
                 {
                     var getTokenSp = _getPageSharepoint.GetAccessToken();
+                    if(getTokenSp == null) { Log.Information("Não foi possível obter token"); }
                     foreach (TBControl ctmodel in pagesFromDB)
                     {
+                        Log.Information("Extraindo dados pagina:" + ctmodel.NOM_PAGI);
                         var content = _getPageSharepoint.MakeExtract(getTokenSp, ctmodel.NOM_PAGI);
                         if (content != null)
                         {
                             DateTime dtModif = Convert.ToDateTime(content.Modified);
                             if ((ctmodel.DAT_ALTE_PAGI != dtModif))
                             {
-
+                                Log.Information("Atualizando tabela de controle");
                                 TBControl tbc = _controlDBRepository.GetItemControl(content);
                                 TBControl tbUpdated = _controlDBRepository.RefreshItem(content, tbc);
                                 _controlDBRepository.UpdateChanges(tbUpdated);
+                                Log.Information("Gerando arquivo");
                                 var file = _fileGenerator.MakeFile(content);
                             }
                             else
                             {
+                                Log.Information("Sem novas informações para:" + ctmodel.NOM_PAGI);
                                 TBControl tbc = _controlDBRepository.GetItemControl(content);
                                 _controlDBRepository.UpdateChanges(tbc);
                             }
@@ -59,6 +71,7 @@ namespace sharepointecs
                 }
                 else
                 {
+                    Log.Information("Não foram encontrados itens na tabela de controle");
                     return;                    
                 }
             }
